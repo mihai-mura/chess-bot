1fd376a7c6e61887f5cc9fa53e866f909bdc4045
Mihai Mura <mihai.mura.dev@gmail.com>
2024-12-02 23:53:45 -0100

    improve evaluation function

--- src/utils/boardEvaluation.ts
+++ src/utils/boardEvaluation.ts
@@ -1,5 +1,94 @@
 import { Chess, Piece, Square } from "chess.js";
 
+const reverseArray = function (array: number[][]) {
+  return array.slice().reverse();
+};
+
+const pawnEvalWhite = [
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  [5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0],
+  [1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0, 1.0],
+  [0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5],
+  [0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0],
+  [0.5, -0.5, -1.0, 0.0, 0.0, -1.0, -0.5, 0.5],
+  [0.5, 1.0, 1.0, -2.0, -2.0, 1.0, 1.0, 0.5],
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+];
+
+const pawnEvalBlack = reverseArray(pawnEvalWhite);
+
+const knightEval = [
+  [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+  [-4.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0, -4.0],
+  [-3.0, 0.0, 1.0, 1.5, 1.5, 1.0, 0.0, -3.0],
+  [-3.0, 0.5, 1.5, 2.0, 2.0, 1.5, 0.5, -3.0],
+  [-3.0, 0.0, 1.5, 2.0, 2.0, 1.5, 0.0, -3.0],
+  [-3.0, 0.5, 1.0, 1.5, 1.5, 1.0, 0.5, -3.0],
+  [-4.0, -2.0, 0.0, 0.5, 0.5, 0.0, -2.0, -4.0],
+  [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+];
+
+const bishopEvalWhite = [
+  [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+  [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+  [-1.0, 0.0, 0.5, 1.0, 1.0, 0.5, 0.0, -1.0],
+  [-1.0, 0.5, 0.5, 1.0, 1.0, 0.5, 0.5, -1.0],
+  [-1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, -1.0],
+  [-1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0],
+  [-1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, -1.0],
+  [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+];
+
+const bishopEvalBlack = reverseArray(bishopEvalWhite);
+
+const rookEvalWhite = [
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  [0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0],
+];
+
+const rookEvalBlack = reverseArray(rookEvalWhite);
+
+const evalQueen = [
+  [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+  [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+  [-1.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0],
+  [-0.5, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5],
+  [0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5],
+  [-1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0],
+  [-1.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, -1.0],
+  [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+];
+
+const kingEvalWhite = [
+  [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+  [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+  [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+  [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+  [-2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+  [-1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+  [2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0],
+  [2.0, 3.0, 1.0, 0.0, 0.0, 1.0, 3.0, 2.0],
+];
+
+const kingEvalBlack = reverseArray(kingEvalWhite);
+
+const squareToIndex = (square: Square) => {
+  const file = square[0] ?? "";
+  const rank = parseInt(square[1] ?? "");
+
+  const x = file.charCodeAt(0) - "a".charCodeAt(0);
+
+  const y = 8 - rank;
+
+  return { x, y };
+};
+
 class BoardEvaluation {
   static evaluatePosition = (game: Chess) => {
     let boardEvaluation = 0;
@@ -7,7 +96,7 @@ class BoardEvaluation {
     for (let rank = 1; rank <= 8; rank++) {
       for (const file of "abcdefgh") {
         const square = `${file}${rank}` as Square;
-        boardEvaluation += this.getSimplePieceValue(game.get(square), square);
+        boardEvaluation += this.getPieceValue(game.get(square), square);
       }
     }
 
@@ -44,48 +133,48 @@ class BoardEvaluation {
     return isWhite ? absoluteValue : -absoluteValue;
   };
 
-  // private static getPieceValue = (piece: Piece, position: Square) => {
-  //   if (!piece) return 0;
+  private static getPieceValue = (piece: Piece, position: Square) => {
+    if (!piece) return 0;
 
-  //   const isWhite = piece.color === "w";
-  //   let absoluteValue = 0;
+    const { x, y } = squareToIndex(position);
+    const isWhite = piece.color === "w";
+    let absoluteValue = 0;
 
-  //   switch (piece.type) {
-  //     case "p":
-  //       absoluteValue =
-  //         10 + (isWhite ? pawnEvalWhite[y][x] : pawnEvalBlack[y][x]);
-  //       break;
+    switch (piece.type) {
+      case "p":
+        absoluteValue =
+          10 + (isWhite ? pawnEvalWhite[y]![x]! : pawnEvalBlack[y]![x]!);
+        break;
 
-  //     case "r":
-  //       absoluteValue =
-  //         50 + (isWhite ? rookEvalWhite[y][x] : rookEvalBlack[y][x]);
-  //       break;
+      case "r":
+        absoluteValue =
+          50 + (isWhite ? rookEvalWhite[y]![x]! : rookEvalBlack[y]![x]!);
+        break;
 
-  //     case "n":
-  //       absoluteValue = 30 + knightEval[y][x];
-  //       break;
+      case "n":
+        absoluteValue = 30 + knightEval[y]![x]!;
+        break;
 
-  //     case "b":
-  //       absoluteValue =
-  //         30 + (isWhite ? bishopEvalWhite[y][x] : bishopEvalBlack[y][x]);
-  //       break;
+      case "b":
+        absoluteValue =
+          30 + (isWhite ? bishopEvalWhite[y]![x]! : bishopEvalBlack[y]![x]!);
+        break;
 
-  //     case "q":
-  //       absoluteValue = 90 + evalQueen[y][x];
-  //       break;
+      case "q":
+        absoluteValue = 90 + evalQueen[y]![x]!;
+        break;
 
-  //     case "k":
-  //       absoluteValue =
-  //         900 + (isWhite ? kingEvalWhite[y][x] : kingEvalBlack[y][x]);
-  //       break;
+      case "k":
+        absoluteValue =
+          900 + (isWhite ? kingEvalWhite[y]![x]! : kingEvalBlack[y]![x]!);
+        break;
 
-  //     default:
-  //       absoluteValue = 0; // Optionally handle unknown piece types
-  //       break;
-  //   }
+      default:
+        throw new Error(`Unknown piece type: ${piece.type}`);
+    }
 
-  //   return isWhite ? absoluteValue : -absoluteValue;
-  // };
+    return isWhite ? absoluteValue : -absoluteValue;
+  };
 }
 
 export default BoardEvaluation;
